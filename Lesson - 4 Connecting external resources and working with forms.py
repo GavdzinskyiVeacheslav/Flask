11d3737f6cc3124a17198378f1bdd11a35538040
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-26 19:12:16 +0300

    Lesson - 4 Connecting external resources and working with forms

---
+++ Lesson - 4 Connecting external resources and working with forms.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+menu = [{"name" : "Installation", "url": "install-flask"},
+        {"name" : "First application", "url": "first-app"},
+        {"name" : "Feedback", "url": "contact" }]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', menu = menu)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = "About the site", menu = menu)
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form['username'])
+    return render_template('contact.html', title = "Feedback", menu = menu)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
