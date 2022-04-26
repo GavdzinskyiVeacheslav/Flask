dbde52dc5ab96bd086f864a6968188e2ae4caa3a
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-26 17:48:20 +0300

    Lesson 2 - Using site page templates

---
+++ Lesson 2 - Using site page templates.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+menu = ["Installation", "First application", "Feedback"]
+
+@app.route('/')
+def index():
+    return render_template('index.html', menu=menu)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About the site", menu=menu)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
