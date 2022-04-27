bad9dd0d9edf84f50012af1fd442661c8de72b69
Gavdzinskyi <veacheslavgavdzynsky@gmail.com>
2022-04-27 21:24:49 +0300

    Lesson_9_A_way_to_present_full_fledged_HTML_pages_on_the_server

--- FDataBase.py
+++ FDataBase.py
@@ -1,6 +1,9 @@
 import math
+import re
 import sqlite3
 import time
+
+from flask import url_for
 
 
 class FDataBase:
@@ -13,15 +16,30 @@ class FDataBase:
         try:
             self.__cur.execute(sql)
             res = self.__cur.fetchall()
-            if res: return res
+            if res:
+                return res
         except:
             print("Error reading from DB")
         return []
 
-    def addPost(self, title, text):
+    def addPost(self, title, text, url):
         try:
+            self.__cur.execute(
+                f"SELECT COUNT() as `count` FROM posts WHERE url LIKE '{url}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("An article with this url already exists")
+                return False
+
+            base = url_for('static', filename='images_html')
+
+            text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                          "\\g<tag>" + base + "/\\g<url>>",
+                          text)
+
             tm = math.floor(time.time())
-            self.__cur.execute('INSERT INTO posts VALUES(NULL, ?, ?, ?)', (title, text, tm))
+            self.__cur.execute(
+                'INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)', (title, text, url, tm))
             self.__db.commit()
         except sqlite3.Error as e:
             print("Error adding article to database"+str(e))
@@ -29,9 +47,10 @@ class FDataBase:
 
         return True
 
-    def getPost(self, postId):
+    def getPost(self, alias):
         try:
-            self.__cur.execute(f"SELECT title, text FROM posts WHERE id = {postId} LIMIT 1")
+            self.__cur.execute(
+                f"SELECT title, text FROM posts WHERE url LIKE '{alias}' LIMIT 1")
             res = self.__cur.fetchone()
             if res:
                 return res
@@ -42,41 +61,12 @@ class FDataBase:
 
     def getPostsAnonce(self):
         try:
-            self.__cur.execute(f"SELECT id, title, text FROM posts ORDER BY time DESC")
+            self.__cur.execute(
+                f"SELECT id, title, text, url FROM posts ORDER BY time DESC")
             res = self.__cur.fetchall()
-            if res: return res
+            if res:
+                return res
         except sqlite3.Error as e:
             print("Error getting article from database "+str(e))
 
         return []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- sq_db.sql
+++ sq_db.sql
@@ -7,5 +7,6 @@ CREATE TABLE IF NOT EXISTS posts (
 id integer PRIMARY KEY AUTOINCREMENT,
 title text NOT NULL,
 text text NOT NULL,
+url text NOT NULL,
 time integer NOT NULL
 );
