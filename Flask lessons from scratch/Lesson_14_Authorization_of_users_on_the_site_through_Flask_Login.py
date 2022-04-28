1b818974ee3d62870d0537686b2acd186f3675a8
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-28 06:57:42 +0300

    Lesson_15_Improving_the_authorization_process

--- Flask lessons from scratch/Lesson_14_Authorization_of_users_on_the_site_through_Flask_Login.py
+++ Flask lessons from scratch/Lesson_14_Authorization_of_users_on_the_site_through_Flask_Login.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '../.env')
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.update(dict(DATABASE=os.path.join(app.root_path, 'Flask.db')))
+app.config.update(dict(DATABASE=os.path.join(app.root_path, '../Flask.db')))
 
 login_manager = LoginManager(app)
 
--- UserLogin.py
+++ UserLogin.py
@@ -1,3 +1,5 @@
+from flask_login import UserMixin
+
 class UserLogin():
 
     def fromDB(self, user_id, db):
@@ -7,15 +9,6 @@ class UserLogin():
     def create(self, user):
         self.__user = user
         return self
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
 
     def get_id(self):
         return str(self.__user['id'])
